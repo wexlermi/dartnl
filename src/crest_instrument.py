2ae6b365521023324455d3446f16cc8e5b8233fe
mwexler <wexlermichaeli@gmail.com>
2013-03-25 22:30:06 -0400

    crest_instrument.py now recursively instruments all .c files in a given directory

--- src/crest_instrument.py
+++ src/crest_instrument.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python -tt
+
+#INSTRUCTIONS FOR USE: Put this file in the directory whose C files
+#                      you want to recursively instrument with CREST declarations.
+#					   Run the file by entering the command:   ./crest_instrument.py
+#					   After this is ran, all the '.c' files in the directory (recursively)
+#                      will be instrumented so they can be ran with CREST.
+
+
 import pdb
 import sys
 import re
 import fileinput
+import os
 
-#typeMap = {'int': 'int', 'short': 'short', 'char': 'char', 'unsigned int': 'unsigned_int', 'unsigned short': 'unsigned_short', 'unsigned char': 'unsigned_char'}
 typeList = ['int', 'short', 'char']
+
 def createCrestStr(x):
-	#pdb.set_trace()
 	spaceTabs = x.group(1)
 	theType = x.group(2)
 	theTypeSplit = theType.split(' ')
@@ -30,61 +38,89 @@ def createCrestStr(x):
 	crestStr = crestStr[:len(crestStr)-1] #remove the extra new line
 	return x.group(0) + crestStr
 
-
-intputFileName = sys.argv[1]
-f = open(intputFileName, 'r')
-text = f.read()
-f.close()
-
-includeStr = '#include <crest.h>'
-if (includeStr not in text):
-	text = includeStr + text
-
-unsignedPattern = r'([\t ])*(unsigned %s)\s+(.*);'
-
-for t in typeList:
-	text = re.sub(unsignedPattern % t, createCrestStr, text)
+def tempForLoopVarReplacement(x):
+	return x.group(1) + x.group(2).upper()
 	
-signedPattern = r'([\t ])*(?<!unsigned )(%s)\s+(.*);'
-for t in typeList:
-	text = re.sub(signedPattern % t, createCrestStr, text)
-#matches = re.findall(pattern, text)
-				#[ -,A-Za-z0-9]
-#pdb.set_trace()
-outputFile = open(intputFileName, 'w')
-outputFile.write(text)
-outputFile.close()
+def undoTempForLoopVarReplacement(x):
+	return x.group(1) + x.group(2).lower()	
 
-#print newText
+def forLoopCrestStr(x):
+	spaceTabs = x.group(1)
+	theType = x.group(2)
+	theTypeSplit = theType.split(' ')
+	crestDec = 'CREST_'
+	if (theTypeSplit[0] == 'unsigned'):
+		crestDec += 'unsigned_' + theTypeSplit[1]
+	else:
+		crestDec += theTypeSplit[0]
+	print x.group()
+	varStr = x.group(3)
+	varStr = varStr.strip()
+	varList = varStr.split(',')
+	varList = [y.strip() for y in varList]
+	crestStr = '\n'
+	for y in varList:
+		equalsIndex = y.find('=')
+		if (equalsIndex != -1):
+			y = y[:equalsIndex]
+		y = y.strip()
+		crestStr += spaceTabs + "\t" + crestDec  + '(' + y + ');\n'
+	crestStr = crestStr[:len(crestStr)-1] #remove the extra new line
+	return x.group(0) + crestStr	
+
+def instrumentFile(fileName):
+	f = open(fileName, 'r')
+	text = f.read()
+	f.close()
+	
+	#get the crest.h include header
+	includeStr = '#include <crest.h>'
+	if (includeStr not in text):
+		text = includeStr + text
+	
+	#temporarily makes all declarations inside of for loop capitalized, so later analysis doesn't interfere
+	skipForLoopPattern = r'(\(\s*)(%s\s*%s)'
+	for t in typeList:
+		text = re.sub(skipForLoopPattern % ('', t), tempForLoopVarReplacement, text)
+		text = re.sub(skipForLoopPattern % ('unsigned', t), tempForLoopVarReplacement, text)
+	
+	#declare all unsigned variables
+	unsignedPattern = r'(?<!\()([\t ]*)(unsigned\s*%s)\s+([^;{]*);'
+	for t in typeList:
+		text = re.sub(unsignedPattern % t, createCrestStr, text)
+	
+	#declare all (implicitly) signed variables
+	signedPattern = r'(?<!\()([\t ]*)(?<!unsigned )(%s)\s+([^;{]*);'
+	for t in typeList:
+		text = re.sub(signedPattern % t, createCrestStr, text)
+
+	#undo the capitalization from before
+	for t in typeList:
+		text = re.sub(skipForLoopPattern % ('', t.upper()), undoTempForLoopVarReplacement, text)
+		text = re.sub(skipForLoopPattern % ('UNSIGNED', t.upper()), undoTempForLoopVarReplacement, text)
+	
+	#crest-declare variables declared in for loop  e.g.    for (int i = 0; i < 10; i++){ }
+	forLoopPattern = r'([\t ]*)for\s*\(\s*(%s\s*%s)\s+([^;]*);[^{]*{'
+	for t in typeList:
+		text = re.sub(forLoopPattern % ('', t), forLoopCrestStr, text)
+		text = re.sub(forLoopPattern % ('unsigned', t), forLoopCrestStr, text)		
+	
+	#write the 'text' variable to the original file
+	outputFile = open(fileName, 'w')
+	outputFile.write(text)
+	outputFile.close()
 
 
+#MAIN ENTRY POINT
 
-'''
-for match in matches:
-	#this part works for "for loop"--- unfinished
-		#if("for" && "int" in line):
-		#i = line.find("int")
-		#j = line.find("=")
-		#var = line[i+4:j]
-		#line = line+1;m
-	text = text.replace(match, match
-	for strs in strings:
-		temp = strs[4:]
-		token = temp.split(',')
-		newstr = strs
-		for tks in token:
-			index = tks.find("=")
-			if index == -1:
-				var = tks
-			else:
-				var = tks[:index]
-			newstr = newstr+';\nCREST_INT('+var+')'
-		
-		if strs in line and "(" not in line:
-			line = line.replace(strs,newstr)
-			break
-		
-	sys.stdout.write(line)
+path = './path'
 
-fileinput.close()
-'''
+#recursively get a list of all C files in this directory
+cFiles = [os.path.join(root, name)
+             for root, dirs, files in os.walk(path)
+             for name in files
+             if name.endswith(".c")]
+for f in cFiles:
+	instrumentFile(f)
+
+
